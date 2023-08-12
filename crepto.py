23dde548134a97a12789a056c0bb2d6694efdbe4
ZiyadZM <ZiyadZabin@gmail>
2023-08-13 02:51:26 +0300

    changed

--- crepto.py
+++ crepto.py
@@ -1,10 +1,19 @@
 import random
 import string
+def print_string(input_string):
+    print("\nHexa:")
+    for char in input_string:
+        print(hex(ord(char)), end=' ')
+    print()
+    print("\nDecimal:")
+    for char in input_string:
+        print(ord(char), end=' ')
+    print()
+
 
 chars = " " + string.punctuation + string.digits + string.ascii_letters
 chars = list(chars)
 key = chars.copy()
-
 random.shuffle(key)
 
 #ENCRYPT
@@ -14,17 +23,18 @@ cipher_text = ""
 for letter in plain_text:
     index = chars.index(letter)
     cipher_text += key[index]
-
 print(f"original message : {plain_text}")
 print(f"encrypted message: {cipher_text}")
+print_string(plain_text)
 
-
+#DECRYPT
 plain_text = ""
-
 for letter in cipher_text:
     index = key.index(letter)
     plain_text += chars[index]
-
-
+print()
+print(f"encrypted message: {cipher_text}")
 print(f"original message : {plain_text}")
+print_string(cipher_text)
+
 
