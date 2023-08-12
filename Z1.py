38f8749edb5df210fbefd56c0199948dbef70d2a
ZiyadZM <ZiyadZabin@gmail>
2023-08-13 00:13:31 +0300

    test 1

--- Z1.py
+++ Z1.py
@@ -1,43 +1,20 @@
-import random
-import math
-# Taking Inputs
-lower = int(input("Enter Lower bound:- "))
+def print_string(input_string):
+    print("Input String:", input_string)
 
-# Taking Inputs
-upper = int(input("Enter Upper bound:- "))
-G = 5
-# generating random number between
-# the lower and upper
-x = random.randint(lower, upper)
-print("\n\tYou've only ",
-	G,
-	" chances to guess the integer!\n")
-print("The random number is ",x)
+    print("\nCharacters:")
+    for char in input_string:
+        print(char, end=' ')
+    print()
 
-# Initializing the number of guesses.
-count = 0
+    print("\nHexa:")
+    for char in input_string:
+        print(hex(ord(char)), end=' ')
+    print()
 
-# for calculation of minimum number of
-# guesses depends upon range
-while count < G :
-	count += 1
+    print("\nDecimal:")
+    for char in input_string:
+        print(ord(char), end=' ')
+    print()
 
-	# taking guessing number as input
-	guess = int(input("Guess a number:- "))
-
-	# Condition testing
-	if x == guess:
-		print("Congratulations you did it in ",
-			count, " try")
-		# Once guessed, loop will break
-		break
-	elif x > guess:
-		print("You guessed too small!")
-	elif x < guess:
-		print("You Guessed too high!")
-
-# If Guessing is more than required guesses,
-# shows this output.
-if count >= G :
-	print("\nThe number is %d" % x)
-	print("\tBetter Luck Next time!")
+input_string = input("Enter a string: ")
+print_string(input_string)
