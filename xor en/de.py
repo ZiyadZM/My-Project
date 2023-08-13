b7ccddd34d9b2c0a2845bc1e1b064e65c09415fc
ZiyadZM <ZiyadZabin@gmail>
2023-08-13 04:18:36 +0300

    changed

--- xor en/de.py
+++ xor en/de.py
@@ -1,23 +1,23 @@
-def xor_encrypt(text, key):
+def xor_encrypt(text):
     encrypted = bytearray()
     for i in range(len(text)):
-        encrypted.append(text[i] ^ key[i % len(key)])
+        encrypted.append(text[i] ^ 1)
     return encrypted
-def xor_decrypt(encrypted, key):
+def xor_decrypt(encrypted):
     decrypted = bytearray()
     for i in range(len(encrypted)):
-        decrypted.append(encrypted[i] ^ key[i % len(key)])
+        decrypted.append(encrypted[i] ^ 1)
     return decrypted
 
 plaintext = "Ziyad"
-encryption_key = b"mykey"
+#encryption_key = b"mykey"
 
-encrypted_data = xor_encrypt(plaintext.encode(), encryption_key)
+encrypted_data = xor_encrypt(plaintext.encode())
 hex_encrypted = encrypted_data.hex()
 print("Encrypted: ",encrypted_data )
 print("Hexadecimal:", hex_encrypted)
 
-decrypted_data = xor_decrypt(encrypted_data, encryption_key)
+decrypted_data = xor_decrypt(encrypted_data)
 hex_decrypted = decrypted_data.hex()
 print("Decrypted: ",decrypted_data.decode())
 print("Hexadecimal:", hex_decrypted)
